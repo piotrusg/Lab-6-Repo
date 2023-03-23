10fd9620f0498e86435d58bd015bf4d43c4c104b
piotrusg <128649381+piotrusg@users.noreply.github.com>
2023-03-22 22:38:37 -0400

    Update Lab-06-Code.py
    fixed password computation

--- Lab-06-Code.py
+++ Lab-06-Code.py
@@ -2,14 +2,21 @@
 
 def encode():
     password = input("Please enter your password to encode: ")
-    encrypted_pass = int(password) + 33333333
+    password = str(password)
+    encrypted_pass = ""
+    for digit in password:
+        encrypted_pass = str(encrypted_pass) + str(((int(digit) + 3) % 10))
+    # this takes the user submitted password, and then adds 3 to each digit in the password
     print("Your password has been encoded and stored!")
     print("")
     return encrypted_pass
 
+
 def decode(encrypted_pass):
     # Emilio Sierra
-    decoded_pass = int(encrypted_pass) - 33333333
+    decoded_pass = ""
+    for digit in encrypted_pass:
+        decoded_pass = str(decoded_pass) + str((int(digit) - 3) % 10)
     return decoded_pass
 
 def main():
@@ -20,12 +27,17 @@ def main():
         option = int(option)
         if option == 1:
             encrypted_pass = encode()
+            # this takes the password provided by the user, and puts it through the encoder, the result of which is
+            # then stored by the program
         if option == 2:
             decoded_pass = decode(encrypted_pass)
             print(f"The encoded password is {encrypted_pass}, and the original password is {decoded_pass}.")
             print("")
+            # this puts the encoded password into the decoder provided by Emilio Sierra, and then states the encoded
+            # and decoded password to the user
         if option == 3:
             break
+            # quits the program
 
 def menu():
     print("Menu")
@@ -34,8 +46,9 @@ def menu():
     print("2. Decode")
     print("3. Quit")
     print("")
+# this prints the menu for the user
+
 
 if __name__ ==  "__main__":
     main()
-
 
